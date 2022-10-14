8cfa1eac3efdb031aa0e6beab570ce0d2ee74172
obkin <kapellwork@gmail.com>
2022-10-14 20:29:07 +0300

    upgrade_v1

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 'use strict';
 
+document.querySelector('.active').addEventListener('click', () => {
+    console.log('click');
+});
+
+console.log('normal');
+
+/*
+
 try {
     console.log('Normal');
     console.log(a);
     console.log('result');
 } catch(error) {
-    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+} finally {
+
 }
 
 console.log('Still normal');
+
+*/
